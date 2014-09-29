3728ec014e3d05708490cadfb02fc44158d0c959
changmoon42 <changmoon42@gmail.com>
2014-09-29 22:47:19 +0900

    Update app.js
    페이지 이동 제거

--- devping-interface/src/main/webapp/WEB-INF/resources/js/app.js
+++ devping-interface/src/main/webapp/WEB-INF/resources/js/app.js
@@ -6,16 +6,16 @@ var devPingApp = angular.module('devPingApp', ['ngRoute']);
 
 devPingApp.config(function ($routeProvider, $httpProvider) {
 	$httpProvider.defaults.headers.post['Content-Type'] = 'application/x-www-form-urlencoded; charset=UTF-8';
-	$routeProvider
-		.when('/main',
-			{
-				controller: 'PingSearchController',
-				templateUrl: urlBase + 'ping-search'
-			})
-		.when('/write',
-			{
-				controller: 'PingQuestionController', 
-				templateUrl: urlBase + 'ping-write'
-			})
-		.otherwise({ redirectTo: '/main' });
+//	$routeProvider
+//		.when('/main',
+//			{
+//				controller: 'PingSearchController',
+//				templateUrl: urlBase + 'ping-search'
+//			})
+//		.when('/write',
+//			{
+//				controller: 'PingQuestionController', 
+//				templateUrl: urlBase + 'ping-write'
+//			})
+//		.otherwise({ redirectTo: '/main' });
 });
