e5f60469ac43a706cfa93d2bd7efc00696d4ca55
changmoon42 <changmoon42@gmail.com>
2014-10-08 01:35:11 +0900

    Create PingToServerRequsetVo.java
    VO 작성

---
+++ devping-domain/src/main/java/org/jbug/devping/vo/PingToServerRequsetVo.java
@@ -0,0 +1,42 @@
+package org.jbug.devping.vo;
+
+import java.util.List;
+
+public class PingToServerRequsetVo extends DataVo {
+	private List<UserVo> userIdsWithTag;
+	private String userId;
+	private String nickName;
+	private String question;
+	
+	public List<UserVo> getUserIdsWithTag() {
+		return userIdsWithTag;
+	}
+	public void setUserIdsWithTag(List<UserVo> userIdsWithTag) {
+		this.userIdsWithTag = userIdsWithTag;
+	}
+	public String getUserId() {
+		return userId;
+	}
+	public void setUserId(String userId) {
+		this.userId = userId;
+	}
+	public String getNickName() {
+		return nickName;
+	}
+	public void setNickName(String nickName) {
+		this.nickName = nickName;
+	}
+	public String getQuestion() {
+		return question;
+	}
+	public void setQuestion(String question) {
+		this.question = question;
+	}
+	
+	@Override
+	public String toString() {
+		return "PingToServerRequsetVo [userIdsWithTag=" + userIdsWithTag
+				+ ", userId=" + userId + ", nickName=" + nickName
+				+ ", question=" + question + "]";
+	}
+}
