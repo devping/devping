969d8479d8ad2a95055ecd82c321bad77b1ab7c7
changmoon42 <changmoon42@gmail.com>
2014-10-08 01:35:25 +0900

    Update UserVo.java

--- devping-domain/src/main/java/org/jbug/devping/vo/UserVo.java
+++ devping-domain/src/main/java/org/jbug/devping/vo/UserVo.java
@@ -3,22 +3,39 @@ package org.jbug.devping.vo;
 import java.util.Set;
 
 public class UserVo {
-    private String name;
-    private Set<String> PersonalTagList;
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public Set<String> getPersonalTagList() {
-        return PersonalTagList;
-    }
-
-    public void setPersonalTagList(Set<String> personalTagList) {
-        PersonalTagList = personalTagList;
-    }
+	private String name;
+	private String userId;
+	private String nickName;
+	private Set<String> PersonalTagList;
+	
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getUserId() {
+		return userId;
+	}
+	public void setUserId(String userId) {
+		this.userId = userId;
+	}
+	public String getNickName() {
+		return nickName;
+	}
+	public void setNickName(String nickName) {
+		this.nickName = nickName;
+	}
+	public Set<String> getPersonalTagList() {
+		return PersonalTagList;
+	}
+	public void setPersonalTagList(Set<String> personalTagList) {
+		PersonalTagList = personalTagList;
+	}
+	
+	@Override
+	public String toString() {
+		return "UserVo [name=" + name + ", userId=" + userId + ", nickName="
+				+ nickName + ", PersonalTagList=" + PersonalTagList + "]";
+	}
 }
