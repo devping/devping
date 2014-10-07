a4f80237939d11543e2b9376df2c3205251d7546
changmoon42 <changmoon42@gmail.com>
2014-10-08 01:34:15 +0900

    Update TagPrefixVo.java

--- devping-domain/src/main/java/org/jbug/devping/vo/TagPrefixVo.java
+++ devping-domain/src/main/java/org/jbug/devping/vo/TagPrefixVo.java
@@ -1,29 +1,29 @@
 package org.jbug.devping.vo;
 
-import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Created by jhouse on 10/7/14.
  */
-public class TagPrefixVo {
-    private String prefix;
-    private ArrayList<TagVo> tagList = new ArrayList<>();
-
-    public String getPrefix() {
-        return prefix;
-    }
-
-    public void setPrefix(String prefix) {
-        this.prefix = prefix;
-    }
-
-    public ArrayList<TagVo> getTagList() {
-        return tagList;
-    }
-
-    public void setTagList(ArrayList<TagVo> tagList) {
-        this.tagList = tagList;
-    }
-
-
+public class TagPrefixVo extends DataVo {
+	private String prefix;
+	private List<String> tagList;
+	
+	public String getPrefix() {
+		return prefix;
+	}
+	public void setPrefix(String prefix) {
+		this.prefix = prefix;
+	}
+	public List<String> getTagList() {
+		return tagList;
+	}
+	public void setTagList(List<String> tagList) {
+		this.tagList = tagList;
+	}
+	
+	@Override
+	public String toString() {
+		return "TagPrefixVo [prefix=" + prefix + ", tagList=" + tagList + "]";
+	}
 }
