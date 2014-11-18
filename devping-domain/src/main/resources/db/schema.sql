7af47bd62df8fdec14b7cc2afa4e5c269f67ca50
ruffneck7 <ruffneck7@gmail.com>
2014-11-18 11:55:31 +0900

    schema 정

--- devping-domain/src/main/resources/db/schema.sql
+++ devping-domain/src/main/resources/db/schema.sql
@@ -1,13 +1,11 @@
 CREATE TABLE `user_accounts` (
 `id` bigint(20) NOT NULL AUTO_INCREMENT,
-`creation_time` datetime NOT NULL,
 `email` varchar(100) NOT NULL,
 `first_name` varchar(100) NOT NULL,
 `last_name` varchar(100) NOT NULL,
-`modification_time` datetime NOT NULL,
 `password` varchar(255),
-`role` varchar(20) NOT NULL,
 `sign_in_provider` varchar(20),
+`tag` varchar(2000),
 `version` bigint(20) NOT NULL,
 PRIMARY KEY (`id`),
 UNIQUE KEY (`email`)
