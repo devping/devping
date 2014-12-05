5f03dc6aba738bf4382991239d827cd63998ffd8
Jooho Lee, Jay <ljhiyh@gmail.com>
2014-12-05 14:54:04 +0900

    Move file location and add some korean comment.

--- devping-domain/src/main/java/org/jbug/devping/service/ChannelService.java
+++ devping-domain/src/main/java/org/jbug/devping/service/ChannelService.java
@@ -1,5 +1,6 @@
-package org.jbug.devping.channel;
+package org.jbug.devping.service;
 
+import org.jbug.devping.channel.PingClientRequestCmd;
 import org.jbug.devping.vo.PingToServerRequsetVo;
 import org.jbug.devping.vo.PingToServerResponseVo;
 import org.jbug.devping.websocket.ChannelManager;
@@ -11,6 +12,7 @@ import java.util.List;
 
 /**
  * Created by jhouse on 10/12/14.
+ * 핑 / 퐁 등 채널관련 커맨드 호출
  */
 @Component
 public class ChannelService {
@@ -19,22 +21,25 @@ public class ChannelService {
     ChannelManager cm;
 
     public PingToServerResponseVo ping(PingToServerRequsetVo pingToServerRequsetVo){
-        //리턴 Vo 생성
+        //핑 요청을 한 것에 대한 리턴 Vo 생성
         PingToServerResponseVo pingToServerResponseVo = new PingToServerResponseVo();
-        //채널 ID 생성
+        //핑을 요청한 사람에서 채널 ID 생성해서 부여
         String channelId = cm.generateChannelId(pingToServerRequsetVo.getUserId());
         String result = "fail";
-        pingToServerResponseVo.setFunc("pingToServer");
+
+        pingToServerResponseVo.setFunc(pingToServerRequsetVo.getFunc()+"_response");
         pingToServerResponseVo.setChannelId(channelId);
         pingToServerResponseVo.setDate((new Date()).toString());
 
         List<String> selectedUserList = pingToServerRequsetVo.getUserIdsWithTag();
         PingClientRequestCmd pingClientRequestCmd;
+
         for(String pongUserId:selectedUserList) {
             pingClientRequestCmd = new PingClientRequestCmd(pingToServerRequsetVo,pongUserId,channelId);
             pingClientRequestCmd.execute();
         }
         result = "success";
+
         pingToServerResponseVo.setResult(result);
 
         return pingToServerResponseVo;
